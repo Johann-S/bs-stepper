7d239635fff1a83e64b3aac6601e90a499f64c3e
XhmikosR <xhmikosr@gmail.com>
2020-07-03 10:05:29 +0300

    Update browsers.js

--- tests/browsers.js
+++ tests/browsers.js
@@ -25,7 +25,7 @@ const browsers = {
     os: 'Windows',
     os_version: '10',
     browser: 'Edge',
-    browser_version: 'latest'
+    browser_version: '15.0'
   },
   ie11Win10: {
     base: 'BrowserStack',
